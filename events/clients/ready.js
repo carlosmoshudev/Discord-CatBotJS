d96687ffaadcd540ea3da2e05ec76ff844b2d1c0
carlosmoshudev <carlos@carlosmoshu.com>
2022-09-02 09:54:13 +0200

    Add commands collection

--- events/clients/ready.js
+++ events/clients/ready.js
@@ -1,5 +1,6 @@
-const { ReadyConfig: config }   = require('../../config/bot.json');
+const { ReadyConfig: config, MessageConfig: message}   = require('../../config/bot.json');
 const clientPresence            = require('../../config/clientPresence');
+const { Loader }                = require('../../utils/Command');
 module.exports                  = client =>
 {
     if(!client) 
@@ -7,9 +8,12 @@ module.exports                  = client =>
         console.error(ReadyConfig.NotClientLog);
         return;
     }
-    const guildsJoined = client.guilds.cache.map(guild => guild.name).join(' \n');
     console.log(config.ReadyLog);
+    const guildsJoined = client.guilds.cache.map(guild => guild.name).join(' \n');
     console.log(config.GuildJoinLog + guildsJoined);
     client.user.setPresence(clientPresence);
+    client.commands = Loader(client);
+    console.log(config.CommandLoadLog + client.commands.map(
+        cmd => message.Prefix + cmd.class.name + ' | ' + cmd.class.description));
 
 }
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
-const { Client }                = require('discord.js');
+const { Client, Collection }    = require('discord.js');
 const { clientEvents: event }   = require('./events/Events');
 const clientOptions             = require('./config/clientOptions');
 
 const client    = new Client(clientOptions);
+
+client.commands = new Collection();
 
 client.on('ready',          client  => event.onReady(client));
 client.on('messageCreate',  message => event.onMessageCreate(message));
