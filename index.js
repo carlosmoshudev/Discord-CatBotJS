1da305ea2ebb43677e30d16899e385dc00f51e7b
carlosmoshudev <carlos@carlosmoshu.com>
2022-09-07 00:15:25 +0200

    pretty

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const { Client, Collection }    = require('discord.js');
-const { clientEvents: event }   = require('./events/Events');
+const { clientEvents }          = require('./events/Events');
 const clientOptions             = require('./config/clientOptions');
 
 const client        = new Client(clientOptions);
 client.commands     = new Collection();
 client.categories   = new Collection();
 
-client.on('ready',          client  => event.onReady(client));
-client.on('messageCreate',  message => event.onMessageCreate(message));
+client.on('ready',          client  => clientEvents.onReady(client));
+client.on('messageCreate',  message => clientEvents.onMessageCreate(message));
 
 client.login(process.env.DISCORD_OAUTH);
