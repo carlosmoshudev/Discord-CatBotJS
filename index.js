1e8b73114216db99bb63f9703bb4a326592ec5fd
carlosmoshudev <carlos@carlosmoshu.com>
2022-09-09 18:48:39 +0200

    Update Logger

--- index.js
+++ index.js
@@ -9,16 +9,9 @@ const client        = new Client(clientOptions);
 client.commands     = new Collection();
 client.categories   = new Collection();
 
-client.on('ready',          client  => 
-{
-    logger.info(ReadyConfig.ReadyLog);
-    clientEvents.onReady(client)
-})
-client.on('messageCreate',  message => 
-{
-    clientEvents.onMessageCreate(message)
-});
-client.on('debug',  msg => logger.debug(msg));
-client.on('warn',   msg => logger.warn(msg));
-client.on('error',  msg => logger.error(msg));
+client.on('ready',          client  => {clientEvents.onReady(client)})
+client.on('messageCreate',  message => {clientEvents.onMessageCreate(message)});
+client.on('debug',          message => logger.debug(message));
+client.on('warn',           message => logger.warn(message));
+client.on('error',          message => logger.error(message));
 client.login(process.env.DISCORD_OAUTH);
--- logger.js
+++ logger.js
@@ -2,7 +2,7 @@ const pino      = require('pino');
 const transport = pino.transport
 ({
     target: 'pino/file',
-    options: { destination: './log.json' },
+    options: { destination: './.Logs/client-log.json' },
 });
 const logger    = pino(transport);
 module.exports  = logger;
