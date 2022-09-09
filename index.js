af8cb4a3a487c8a7e1204ed1c9c321535525c003
carlosmoshudev <carlos@carlosmoshu.com>
2022-09-09 17:17:41 +0200

    Add pino logger ./log.json

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ require('dotenv').config();
 const { Client, Collection }    = require('discord.js');
 const { clientEvents }          = require('./events/Events');
 const clientOptions             = require('./config/clientOptions');
+const logger                    = require('./logger');
+const { ReadyConfig } = require('./config/bot.json');
 
 const client        = new Client(clientOptions);
 client.commands     = new Collection();
 client.categories   = new Collection();
 
-client.on('ready',          client  => clientEvents.onReady(client));
-client.on('messageCreate',  message => clientEvents.onMessageCreate(message));
-
+client.on('ready',          client  => 
+{
+    logger.info(ReadyConfig.ReadyLog);
+    clientEvents.onReady(client)
+})
+client.on('messageCreate',  message => 
+{
+    clientEvents.onMessageCreate(message)
+});
+client.on('debug',  msg => logger.debug(msg));
+client.on('warn',   msg => logger.warn(msg));
+client.on('error',  msg => logger.error(msg));
 client.login(process.env.DISCORD_OAUTH);
---
+++ logger.js
@@ -0,0 +1,8 @@
+const pino      = require('pino');
+const transport = pino.transport
+({
+    target: 'pino/file',
+    options: { destination: './log.json' },
+});
+const logger    = pino(transport);
+module.exports  = logger;
