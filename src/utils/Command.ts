c968db2263e5bb69afefcb3bd0faca3d3100b206
carlosmoshudev <carlos@carlosmoshu.com>
2022-09-22 09:24:19 +0200

    Type refactor

--- src/utils/Command.ts
+++ src/utils/Command.ts
@@ -2,23 +2,31 @@ import { Collection, Client } from 'discord.js';
 import { readdirSync } from 'fs';
 import { Command } from '../models/Command';
 
-type CommandAndAlias = { commands: Collection<string, Command>, aliases: Collection<string, Command> };
+type CommandAndAlias = {
+    commands: Collection<string, Command>,
+    aliases: Collection<string, Command>
+};
 
-const directory: string = `${process.cwd()}/src/commands/`;
-const fileExtension: string = '.ts'
+const
+    directory: string = `${process.cwd()}/src/commands/`,
+    fileExtension: string = '.ts'
 
 export function CommandLoader(client: Client): CommandAndAlias {
     let commandsInfo: Collection<string, Command> = new Collection();
-    let aliasesInfo: Collection<string,Command> = new Collection();
-    readdirSync(directory).forEach(category => readdirSync(`${directory}${category}`).forEach(command => {
-        if (command.endsWith(fileExtension)) {
-            let commandBuilder: any = require(`${directory}${category}/${command}`);
-            const cmd: Command = new commandBuilder.ConcreteCommand(client);
-            commandsInfo.set(cmd.name, cmd);
-            cmd.aliases.forEach(alias => aliasesInfo.set(alias, cmd));
-        }
-    }));
-    return { commands: commandsInfo, aliases: aliasesInfo }
+    let aliasesInfo: Collection<string, Command> = new Collection();
+    readdirSync(directory).forEach(category =>
+        readdirSync(`${directory}${category}`).forEach(command => {
+            if (command.endsWith(fileExtension)) {
+                let commandBuilder = require(`${directory}${category}/${command}`);
+                const cmd: Command = new commandBuilder.ConcreteCommand(client);
+                commandsInfo.set(cmd.name, cmd);
+                cmd.aliases.forEach(alias => aliasesInfo.set(alias, cmd));
+            }
+        }));
+    return {
+        commands: commandsInfo,
+        aliases: aliasesInfo
+    }
 }
 
 export function CategoryLoader(): Collection<string, string> {
