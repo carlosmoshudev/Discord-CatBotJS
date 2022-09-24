d20c1f215b8133aefcd50dffb717dd4aa8481528
carlosmoshudev <carlos@carlosmoshu.com>
2022-09-24 03:29:04 +0200

    Add Channel data type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,2 +1,12 @@
 export type NameValue = { name: string, value: string };
-export type Color = { yellow: string, cyan: string, purple: string }; 
+export type Color = { yellow: string, cyan: string, purple: string };
+export type CommandAndAlias = {
+    commands: Collection<string, Command>,
+    aliases: Collection<string, Command>
+};
+export type CreateChannelData = {
+    message: Message,
+    name: string,
+    parent: CategoryChannelResolvable,
+    type: number
+}
