b99ad31826a6457303bee43d10ef3e30f86d3811
Carlos Moshu <carlos@carlosmoshu.com>
2022-09-24 11:26:21 +0200

    Feature\welcome event (#8)
    * Add channels for welcome message
    
    * Add type and interfaces for serverchannels
    
    * Export get channels from cache by id
    
    * Send welcome message and log when memeber joined
    
    * Add events for memeber add or leave
    
    * Add channels for welcome message
    
    * Add type and interfaces for serverchannels
    
    * Export get channels from cache by id
    
    * Send welcome message and log when memeber joined
    
    * Add events for memeber add or leave

--- src/events/guilds/memberAdd.ts
+++ src/events/guilds/memberAdd.ts
@@ -1,18 +1,60 @@
 import {
+    ColorResolvable,
     EmbedBuilder,
     GuildBasedChannel,
-    GuildMember,
-    TextChannel
+    GuildMember
 } from 'discord.js';
 import Servers from '../../config/servers.json';
+import { EmbedDecorator } from '../../config/decorator.json'
+import { ServerChannels } from '../../types';
+import { TextChannelOnCache } from '../../utils/Channels';
 
 export async function MemberAdd(member: GuildMember): Promise<void> {
     if (!Servers.hasOwnProperty(member.guild.id!)) return;
     const
-        logChannelID: string = Servers[member.guild.id!].channels.logs.welcome,
-        logChannel: GuildBasedChannel = member.guild?.channels.cache.get(logChannelID)! as TextChannel,
+        currentServerChannels: ServerChannels = Servers[member.guild.id!].channels!;
+    const //TODO: Refactorizar a objeto
+        logChannelID: string = currentServerChannels.logs.welcome,
+        welcomeChannelID: string = currentServerChannels.post.welcome,
+        introduceID: string = currentServerChannels.share.introduce,
+        rulesID: string = currentServerChannels.post.rules,
+        autorolesID: string = currentServerChannels.share.autoroles;
+    const
+        logChannel: GuildBasedChannel = TextChannelOnCache(member, logChannelID),
+        welcomeChannel: GuildBasedChannel = TextChannelOnCache(member, welcomeChannelID),
+        introduceChannel: GuildBasedChannel = TextChannelOnCache(member, introduceID),
+        rulesChannel: GuildBasedChannel = TextChannelOnCache(member, rulesID),
+        autorolesChannel: GuildBasedChannel = TextChannelOnCache(member, autorolesID);
+    const
         user: string = `${member.user?.username}#${member.user?.discriminator}`,
-        avatar: string = member.avatarURL()!,
-        embedReply: EmbedBuilder = new EmbedBuilder().setTitle('Demos la bienvenida a nosequien!')
-    console.log(logChannel, user, avatar, embedReply);
+        avatar: string = member.user.avatarURL()!,
+        logEmbed: EmbedBuilder = new EmbedBuilder(),
+        welcomeEmbed: EmbedBuilder = new EmbedBuilder();
+
+    welcomeEmbed
+        .setTitle(`Damos la bienvenida a ${user}`)
+        .setDescription(`Es un placer que te unas ${member.nickname}!`)
+        .addFields(
+            [
+                {
+                    name: `Preséntate`, value: `${introduceChannel}`, inline: true
+                },
+                {
+                    name: `Reglas`, value: `${rulesChannel}`, inline: true
+                },
+                {
+                    name: `Roles`, value: `${autorolesChannel}`, inline: true
+                }
+            ])
+        .setThumbnail(avatar!)
+        .setColor(EmbedDecorator.color as ColorResolvable);
+
+    logEmbed
+        .setTitle(`${user} se ha unido al servidor.`)
+        .setTimestamp()
+        .setThumbnail(avatar!)
+        .setColor(EmbedDecorator.color as ColorResolvable);
+
+    welcomeChannel.send({ embeds: [welcomeEmbed] });
+    logChannel.send({ embeds: [logEmbed] });
 }
